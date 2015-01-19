e46827da3e6dfa818f15354637d405e079460ded
Celeste Layne <layne.celeste@gmail.com>
2015-01-18 17:30:14 -0800

    Initial Commit

---
+++ tippr/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tippr
+//
+//  Created by Celeste Layne on 1/18/15.
+//  Copyright (c) 2015 Celeste Layne. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
